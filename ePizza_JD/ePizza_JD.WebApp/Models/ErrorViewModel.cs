fc514817bc22cdab18cb800dd426b53a2ff77bfb
unknown <jonas.anne@outlook.com>
2020-10-23 09:57:56 +0200

    new project, seperate class library + identity framworkcore installed + Models created

---
+++ ePizza_JD/ePizza_JD.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ePizza_JD.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
